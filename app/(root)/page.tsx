48ac8c343c6ffe1ea6b8a547bf574b875dbd62b7
Dalibor Belic <dalibor@kulturniautomat.com>
2024-10-31 13:30:53 +0100

    Add Logo text

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import ContactForm from '../../components/ContactForm'
+import Logo from '../../components/Logo'
 import Spacer from '../../components/Spacer'
 import Footer from './components/Footer'
 import Process from './components/Process'
@@ -12,10 +13,8 @@ const HomePage: React.FC = () => {
     <div className="min-h-screen md:h-screen md:overflow-hidden flex flex-col-reverse md:flex-row w-full">
       {/* Left scrollable panel */}
       <div className="w-full md:w-1/2 md:overflow-y-auto md:overflow-hidden px-small pb-extrasmall md:px-medium pt-medium">
-        {/* future logo */}
-        <div className="title md:block hidden">TisaCode</div>
-        <Spacer size="large" className="md:block hidden" />
-        <Spacer size="small" className="md:hidden block" />
+        <Logo />
+        <Spacer size="large" className="md:block " />
         <div className="hero-message">
           We craft clean, scalable code that grows with your business, delivering straightforward,
           maintainable solutions that drive real results for you.
@@ -36,9 +35,6 @@ const HomePage: React.FC = () => {
       </div>
 
       {/* Right static panel */}
-      {/* <div className="w-full md:w-1/2 p-6 bg-black">
-        <div className="action">Software is a service, not a commodity.</div>
-      </div> */}
       <div className="relative w-full md:w-1/2 px-small py-medium md:p-medium">
         <Image
           src="/images/background.jpg"
