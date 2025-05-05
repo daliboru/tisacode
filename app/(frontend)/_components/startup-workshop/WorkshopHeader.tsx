34a7f943cbfbe60f2641e4b9ad4e690355070755
Dalibor Belic <dalibor@kulturniautomat.com>
2025-05-05 10:07:10 +0200

    docs(WorkshopHeader): fix typo in hero message text
    Correct the punctuation in the hero message to improve readability and ensure proper grammar

--- app/(frontend)/_components/startup-workshop/WorkshopHeader.tsx
+++ app/(frontend)/_components/startup-workshop/WorkshopHeader.tsx
@@ -12,7 +12,7 @@ const WorkshopHeader: React.FC = () => {
       <div className="font-bold">A 1-hour online workshop + Q&A on May 15th </div>
 
       <div className="hero-message">
-        {`How to move past superficial barrier of communicating problems and address root of why up to 85% of tech projects fail with scalable, sustainable solutions. Don’t just solve what users ask for—solve what they need. Engineer lean systems that turn those needs into quantifiable business gains.`}
+        {`How to move past superficial barrier of communicating problems and address the root of why up to 85% of tech projects fail with scalable, sustainable solutions? Don’t just solve what users ask for—solve what they need. Engineer lean systems that turn those needs into quantifiable business gains.`}
       </div>
     </>
   )
